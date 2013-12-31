3677153bec0604b30965845335e03b013c20ca19
allenhu <allenhu@sohu-inc.com>
2013-12-31 10:02:41 +0800

    添加quickdeploy接口

--- essay/tasks/deploy.py
+++ essay/tasks/deploy.py
@@ -5,7 +5,7 @@ from fabric.state import env
 
 from essay.tasks import virtualenv, supervisor, package, build
 
-__all__ = ['deploy']
+__all__ = ['deploy', 'quickdeploy']
 
 
 @task(default=True)
@@ -27,3 +27,23 @@ def deploy(version, venv_dir, profile):
         package.install(env.PROJECT, version)
         supervisor.shutdown()
         supervisor.start()
+
+
+@task(default=True)
+def quickdeploy(venv_dir, profile, branch=None):
+    """
+    快速部署
+
+        $ fab -R yourroles quickdeploy:a,test,master
+    """
+
+    deploy_host_string = env.host_string
+
+    build_host = env.roledefs.get('build')
+    env.host_string = build_host[0] if isinstance(build_host, list) else build_host
+    build.build(branch=branch)
+
+    env.host_string = deploy_host_string
+    version = build.get_latest_version()
+
+    deploy(version, venv_dir, profile)
